683a7b8a7b6b9642f03b73d9d15eb63361a19f94
Angel Mora <eduardo.aemc@gmail.com>
2017-03-16 03:44:24 -0500

    add the logic the system

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,14 +13,61 @@ class HomeController < ApplicationController
 
 	if last_number == "1" || last_number == "2"
 		deneid_day = "Lunes"
+		if deneid_day == day
+			if (permit_hour >= 700 && permit_hour <= 930) || (permit_hour >= 1600 && permit_hour <= 1930)
+				@failed = "Mala suerte!, No puedes salir en tu coche a esta hora"
+			else
+				@success = "Enhorabuena!, Puedes salir en tu coche"
+			end
+		else
+			@success = "Enhorabuena!, Puedes salir en tu coche"
+		end
 	elsif last_number == "3" || last_number == "4"
 		deneid_day = "Martes"
+		if deneid_day == day
+			if (permit_hour >= 700 && permit_hour <= 930) || (permit_hour >= 1600 && permit_hour <= 1930)
+				@failed = "Mala suerte!, No puedes salir en tu coche a esta hora"
+			else
+				@success = "Enhorabuena!, Puedes salir en tu coche"
+			end
+		else
+			@success = "Enhorabuena!, Puedes salir en tu coche"
+		end
 	elsif last_number == "5" || last_number == "6"
 		deneid_day = "Miercoles"
+		if deneid_day == day
+			if (permit_hour >= 700 && permit_hour <= 930) || (permit_hour >= 1600 && permit_hour <= 1930)
+				@failed = "Mala suerte!, No puedes salir en tu coche a esta hora"
+			else
+				@success = "Enhorabuena!, Puedes salir en tu coche"
+			end
+		else
+			@success = "Enhorabuena!, Puedes salir en tu coche"
+		end
 	elsif last_number == "7" || last_number == "8"
 		deneid_day = "Jueves"
+		if deneid_day == day
+			if (permit_hour >= 700 && permit_hour <= 930) || (permit_hour >= 1600 && permit_hour <= 1930)
+				@failed = "Mala suerte!, No puedes salir en tu coche a esta hora"
+			else
+				@success = "Enhorabuena!, Puedes salir en tu coche"
+			end
+		else
+			@success = "Enhorabuena!, Puedes salir en tu coche"
+		end
 	elsif last_number == "9" || last_number == "0"
 		deneid_day = "Viernes"
+		if deneid_day == day
+			if (permit_hour >= 700 && permit_hour <= 930) || (permit_hour >= 1600 && permit_hour <= 1930)
+				@failed = "Mala suerte!, No puedes salir en tu coche a esta hora"
+			else
+				@success = "Enhorabuena!, Puedes salir en tu coche"
+			end
+		else
+			@success = "Enhorabuena!, Puedes salir en tu coche"
+		end
+	else
+		redirect_to root_path
 	end
   end
 end
