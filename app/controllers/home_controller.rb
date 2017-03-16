6c4b26608a7b4c1e5f62ffee5137006f3f41ece4
Angel Mora <eduardo.aemc@gmail.com>
2017-03-16 03:34:06 -0500

    insert the deneid_day

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,5 +9,18 @@ class HomeController < ApplicationController
 
 	last_number = plate.to_s.last
 	permit_hour = hour.delete(":").to_i
+
+
+	if last_number == "1" || last_number == "2"
+		deneid_day = "Lunes"
+	elsif last_number == "3" || last_number == "4"
+		deneid_day = "Martes"
+	elsif last_number == "5" || last_number == "6"
+		deneid_day = "Miercoles"
+	elsif last_number == "7" || last_number == "8"
+		deneid_day = "Jueves"
+	elsif last_number == "9" || last_number == "0"
+		deneid_day = "Viernes"
+	end
   end
 end
